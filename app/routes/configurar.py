25cb54f5e457032803f5ba7f35ec0659125aea8b
Jesus Bocanegra <jesusbocd@gmail.comm>
2025-05-10 19:41:16 -0500

    Añadida la ruta y la lógica para crear nuevos grados

--- app/routes/configurar.py
+++ app/routes/configurar.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from app.utils.functions import commit
 from app import db
-from app.models.connections import Nivel
+from app.models.connections import Nivel, Grado
 
 config_bp = Blueprint('cursos',__name__, url_prefix='/configurar')
 
@@ -15,3 +15,16 @@ def crear_nivel():
     return commit(nuevo_nivel,
            "Nivel creado correctamente",
            "No se pudo crear el nivel")
+
+@config_bp.route('/crear_grado', methods=['POST'])
+def crear_grado():
+    nivel_id = request.form.get('nivel')
+    nombre = request.form.get('nombre')
+    if not nombre or not nivel_id:
+        return jsonify({'success': False, 'message': 'El nombre y el nivel son requeridos.'}), 400
+    
+    grado = Grado(nombre=nombre,nivel_id=nivel_id)
+    return commit(grado,
+                  "Grado creado correctamente",
+                  "No pudo crearse el grado",
+                  )
